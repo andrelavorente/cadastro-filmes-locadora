739baf03b288ed94ffab8ede0498739ce1ecfa78
Andre Lavorente <lavorente.a@gmail.com>
2023-09-28 09:21:37 -0300

    ATT

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({extended: false}))
 
 app.use(express.static('./publico'))
 
-app.use('./login', rotaLogin)
+app.use('/login', rotaLogin)
 app.use(autenticar, express.static('./protegido'))
 
 
