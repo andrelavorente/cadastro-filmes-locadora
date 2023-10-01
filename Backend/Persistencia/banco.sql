e071a994b877f17b49d920de15559a1eb8e2f329
andrelavorente <lavorente.a@gmail.com>
2023-10-01 17:07:55 -0300

    Update banco.sql

--- Backend/Persistencia/banco.sql
+++ Backend/Persistencia/banco.sql
@@ -1,7 +1,7 @@
 CREATE TABLE filme (
-  id VARCHAR (10) NOT NULL PRIMARY KEY,
   nomeFilme VARCHAR (100) NOT NULL,
   ano VARCHAR(10),
+  id VARCHAR (10) NOT NULL PRIMARY KEY,
   categoria VARCHAR (20),
   palavra VARCHAR (300)
 )
